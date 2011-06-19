2174b3008270450e3d9ca367a65b9dacd064a7d0
Patrick Godbey <mrpatg@gmal.com>
2011-06-19 05:25:37 -0400

    Fixed blank file errors within zip file. Added a couple actions on various conditions.

---
+++ filetemplates.php
@@ -0,0 +1,39 @@
+<?
+
+function fileTemplates($shortcut){
+
+	if($shortcut == "html5"){
+	$html5 = '<!doctype html>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	
+	<title></title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+</head>
+<body>
+
+
+</body>
+</html>';
+	return $html5;
+	}
+	
+	if($shortcut == "960"){
+	$css960 = "fileplacement/css/960.css";
+	return $css960;
+	}
+
+}
+
+?>
---
+++ test.php
@@ -0,0 +1,89 @@
+<?
+require("filetemplates.php");
+require("fileplacements.php");
+$zipfilename = $_POST['name'];
+$dirs = explode("\n", $_POST[config]);
+
+$zip = new ZipArchive;
+if ($zip->open('cache/'.$zipfilename, ZipArchive::CREATE) === TRUE) {
+
+	foreach ($dirs as $key => $value){
+		if(strstr($value, "/")){
+			$subdir = explode("/", $value);
+			if($subdir){
+				if(strstr($value, ".") && strstr($value, ":")){
+				//	echo "$value is a file template<br/>";
+				$file = explode(":", $value);
+				$filename = $file[0];
+				$filetemplate = fileTemplates($file[1]);
+				$zip->addFile($filetemplate, $filename);
+				}else{		
+					if(strstr($value, ".")){
+					//	echo "$value is a file<br/>";
+					}else{
+						if(strstr($value, ":")){
+						//	echo "$value is a file placement<br/>";
+						}else{
+							// This is a directory
+							$value=trim($value);
+							$zip->addEmptyDir("$value"); 
+						}
+					}
+				}
+			}
+		}else{
+			if(strstr($value, ".") && strstr($value, ":")){
+			//	echo "$value is a file template<br/>";
+				$file = explode(":", $value);
+				$filename = $file[0];
+				$filetemplate = fileTemplates($file[1]);
+				$zip->addFromString("$filename", "$filetemplate");
+			}else{
+				if(strstr($value, ".")){
+				//	echo "$value is a file<br/>";
+				$value=trim($value);
+					$zip->addFromString("$value", '');
+				}else{
+					if(strstr($value, ":")){
+				//		echo "$value is a file placement<br/>";
+					}
+				}
+			}
+		}
+	}
+
+//	$zip->addEmptyDir("dir"); 
+//  $zip->addFromString('dir/test.txt', 'file content goes here');
+    $zip->close();
+
+	header("Content-type: Content-Type: application/octet-stream");
+	header("Content-Disposition: attachment; filename=$zipfilename");
+	header("Pragma: no-cache");
+	header("Expires: 0");
+	readfile("cache/$zipfilename");
+	unlink("cache/$zipfilename");
+
+	exit;
+
+} else {
+	echo 'failed';
+}
+
+
+
+
+
+
+
+
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+?>
+
+
+<form action=test.php method=POST>
+<input type=text name=name><br/>
+<textarea name=config></textarea>
+<input type=submit>
+</form>
